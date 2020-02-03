84e33c3326c93eca14afdaa4a7741ce79e119264
timothyalton <mitchell.alton@live.com>
2020-02-03 17:50:01 -0600

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,57 @@
 class Startup
 
+    attr_reader :founder, :domain
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def pivot(name, domain)
+        @name = name
+        @domain = domain 
+    end
+
+    def self.find_by_founder(name)
+        @@all.find {|startup| startup.founder == name}
+    end
+
+    def self.domains
+        @@all.map {|startup| startup.domain}
+    end
+
+
+    def fundings
+        FundingRound.all.select {|funding| funding.startup == self}
+    end
+    
+    def num_funding_rounds
+        fundings.count
+    end
+
+    def total_funds
+        # fundings.map {|funding| funding.investment}.sum 
+        fundings.reduce(0) {|sum, f| sum + f.investment}
+
+    end
+
+    def investors
+        fundings.map {|funding| funding.venture_capitalist.name}.uniq
+    end
+
+    def big_investors
+        # fundings.select {|funding| funding.venture_capitalist}
+        fundings.select{|funding| VentureCapitalist.tres_comma_club.include?(funding.venture_capitalist)}
+    end
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,29 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+s1 = Startup.new("Start Up One", "Founder One", "StartUpOne.com")
+s2 = Startup.new("Start Up Two", "Founder Two", "StartUpTwo.com")
+s3 = Startup.new("Start Up Three", "Founder One", "StartUpThree.com")
+s4 = Startup.new("Start Up Four", "Founder Four", "StartUpFour.com")
+s5 = Startup.new("Start Up Five", "Founder Five", "StartUpFive.com")
+
+v1 = VentureCapitalist.new("VC One", 5000000)
+v2 = VentureCapitalist.new("VC Two", 15000000)
+v3 = VentureCapitalist.new("VC Three", 20000000)
+v4 = VentureCapitalist.new("VC Four", 2500000000)
+v5 = VentureCapitalist.new("VC Five", 500000000)
+v6 = VentureCapitalist.new("VC Six", 1000000001)
+
+f1 = FundingRound.new(s1, v1, "Angel", 500000)
+f2 = FundingRound.new(s2, v6, "Seed", 1000000)
+f3 = FundingRound.new(s3, v4, "Series A", 5000000)
+f4 = FundingRound.new(s1, v5, "Pre-Seed", 50000000)
+f5 = FundingRound.new(s4, v3, "Series B", 5000000)
+f6 = FundingRound.new(s5, v2, "Angel", 1000000)
+f7 = FundingRound.new(s1, v2, "Pre-Seed", 2000000)
+f8 = FundingRound.new(s1, v2, "Pre-Seed", 2000000)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
