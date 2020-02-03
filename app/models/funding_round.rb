ab06852f84d24b285abe3ccc0d951c5776e0455c
corn-cloud <comowaiye@gmail.com>
2020-02-03 17:20:54 -0600

    done

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,17 @@
+
 class FundingRound
+
+  attr_accessor :startup, :venture, :type, :investment
+
+  @@all = []
+
+  def initialize(startup, venture, type, investment)
+    @startup, @venture, @type, @investment  = startup, venture, type, investment
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end  
 
 end
