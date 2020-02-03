3bc970986d2ab547af6c61b5314156036cabb12e
DrVonDevious <drvondevious@gmail.com>
2020-02-03 17:08:17 -0600

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,14 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+s1 = Startup.new("Random Startup", "Tommy", "www.randomstartup.com")
+
+v1 = VentureCapitalist.new("Ashton", 10000526009)
+v2 = VentureCapitalist.new("Todd", 10006869)
+
+fr1 = FundingRound.new(s1, v1, "Seed", 10000)
+fr2 = FundingRound.new(s1, v2, "Seed", 1434200)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
