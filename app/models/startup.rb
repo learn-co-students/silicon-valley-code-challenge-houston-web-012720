9eeb7eb363c0680b0d7e1c1a84faf1859849e31c
placidowang <placidow@gmail.com>
2020-02-03 18:00:27 -0600

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,54 @@
 class Startup
+    attr_reader :founder, :domain, :name
+
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self,venture_capitalist, type, investment)
+    end 
+    
+    def pivot(domain, name)
+        @name = name
+        @domain = domain
+    end
+
+    def self.find_by_founder(name)
+        @@all.find {|founder| founder.name == name}
+    end
+
+    def self.domains
+        @@all.map {|startup| startup.domain}
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|funding| funding.startup == self}
+    end
+
+    def num_funding_rounds
+        funding_rounds.count
+    end
+
+    def total_funds
+        funding_rounds.reduce(0) {|sum, funding| sum + funding.investment}
+    end
+
+    def investors
+        funding_rounds.map {|funding| funding.venture_capitalist}.uniq
+    end
+
+    def big_investors
+        investors.select {|investor| VentureCapitalist.tres_commas_club.include?(investor)}
+    end
 
 end
